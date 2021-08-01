42e756b0981b3c7f14eeb747e218820b88f24f60
jozef-wolf <wolf.joozef@gmail.com>
2021-08-01 16:08:57 +0200

    handle click event

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,16 @@ const home = () => {
     console.log("Hello!");
   };
 
+  const handleClickAgain = (name) => {
+    console.log("Hello again " + name);
+  };
+
   return (
     <div>
       <h2>Homepage</h2>
       <p></p>
       <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain("Josef")}>Click me again</button>
     </div>
   );
 };
