8cef874cd6ada17df3ce6013013f472c3086fbd7
jozef-wolf <wolf.joozef@gmail.com>
2021-08-02 13:28:38 +0200

    Conditional Loading Message

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 const Home = () => {
   const [todos, setTodos] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
@@ -12,12 +13,14 @@ const Home = () => {
       .then((data) => {
         console.log(data);
         setTodos(data);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <div>
       <h2>Homepage</h2>
+      {isLoading && <div>Loading...</div>}
       {todos && <TodoList todos={todos} />}
     </div>
   );
