dcfef4c29d043b34c8130afbc522d9c318eb34e5
jozef-wolf <wolf.joozef@gmail.com>
2021-08-02 13:59:42 +0200

    Custom hook -useFetch hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,13 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import TodoList from "./TodoList";
+import useFetch from "./useFetch";
+
 const Home = () => {
-  const [todos, setTodos] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("could not fetch the data for that resource");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setTodos(data);
-        setIsLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        setError(err.message);
-      });
-  }, []);
+  const {
+    data: todos,
+    isLoading,
+    error,
+  } = useFetch("https://jsonplaceholder.typicode.com/posts");
 
   return (
     <div>
