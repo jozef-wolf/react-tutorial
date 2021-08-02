2cf6cde83be794815a48e0774c1ae1e09ed38bae
jozef-wolf <wolf.joozef@gmail.com>
2021-08-02 15:56:28 +0200

    Route Links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
       <h1>Navigation</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create">Add new todo</Link>
+      </div>
     </div>
   );
 };
