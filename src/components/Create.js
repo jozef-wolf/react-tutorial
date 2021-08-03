01685b0409e592b567a8ba8402b0390cc34ec887
jozef-wolf <wolf.joozef@gmail.com>
2021-08-03 12:25:02 +0200

    Form - submit event

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("User");
   return (
     <div className="create">
       <h2>Add a New Todo</h2>
+      <form>
+        <label>Todo Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+      </form>
+      <form>
+        <label>Todo Body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="User">User</option>
+          <option value="Anonymous">Anonymous</option>
+        </select>
+        <button>Add todo</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
+      </form>
     </div>
   );
 };
