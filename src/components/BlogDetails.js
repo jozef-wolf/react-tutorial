ea5a0f2b5a8d218f385b6c3564f8f0eb37fd05f1
jozef-wolf <wolf.joozef@gmail.com>
2021-09-22 12:04:26 +0200

    blog alignment style

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -20,16 +20,18 @@ const BlogDetails = () => {
 
   return (
     <div className="blog-details">
-      {isPending && <div>Loading...</div>}
-      {error && <div>{error}</div>}
-      {blog && (
-        <article>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-          <div>{blog.body}</div>
-          <button onClick={handleClick}>delete</button>
-        </article>
-      )}
+      <div>
+        {isPending && <div>Loading...</div>}
+        {error && <div>{error}</div>}
+        {blog && (
+          <article>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+            <div>{blog.body}</div>
+            <button onClick={handleClick}>delete</button>
+          </article>
+        )}
+      </div>
     </div>
   );
 };
